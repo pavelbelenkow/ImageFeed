0397246bd72fba977db7af01034ba6e37a44d109
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2023-04-18 02:58:07 +0300

    Minor file shifts

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Pavel Belenkow on 13.04.2023.
+//
+
+import Foundation
+
+enum Constants: String {
+    case accessKey = "GCKaAeFPPUB9npUECl2AqtV9TEj9shAu-DF6TnYzjAs"
+    case secretKey = "nXkygOWaPszuKpXqIScJKRiBM-l5HmosLDcfPQN_kfw"
+    case redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    case accessScope = "public+read_user+write_likes"
+    case defaultBaseURL = "https://api.unsplash.com"
+    case unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
