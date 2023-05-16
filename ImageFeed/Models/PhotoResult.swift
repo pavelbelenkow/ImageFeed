32c3c89a8d7e22ca5c56b62383132e070bc3f149
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2023-05-16 15:49:15 +0300

    Implemented ImagesList service

---
+++ ImageFeed/Models/PhotoResult.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoResult.swift
+//  ImageFeed
+//
+//  Created by Pavel Belenkow on 16.05.2023.
+//
+
+import Foundation
+
+struct PhotoResult: Decodable {
+    let id: String
+    let createdAt: String
+    let width: Int
+    let height: Int
+    let likedByUser: Bool
+    let description: String?
+    let urls: UrlsResult
+}
+
+struct UrlsResult: Decodable {
+    let raw: String
+    let full: String
+    let regular: String
+    let small: String
+    let thumb: String
+}
---
+++ ImageFeed/Services/ImagesListService.swift
@@ -0,0 +1,93 @@
+//
+//  ImagesListService.swift
+//  ImageFeed
+//
+//  Created by Pavel Belenkow on 16.05.2023.
+//
+
+import Foundation
+
+final class ImagesListService: ImagesListServiceProtocol {
+    
+    static let shared = ImagesListService()
+    static let didChangeNotification = Notification.Name(rawValue: "ImagesListServiceDidChange")
+    
+    private let session = URLSession.shared
+    private let token = OAuth2TokenStorage.shared.token
+    private(set) var photos: [Photo] = []
+    private var lastLoadedPage: Int?
+    private var task: URLSessionTask?
+    
+    func fetchPhotosNextPage(completion: @escaping (Result<Photo, Error>) -> Void) {
+        assert(Thread.isMainThread)
+        if task != nil {
+            task?.cancel()
+            return
+        }
+    
+        guard let request = photoRequest() else { return }
+        
+        let task = session.objectTask(for: request) { [weak self] (result: Result<PhotoResult, Error>) in
+            guard let self else { return }
+            
+            switch result {
+            case .success(let photoResult):
+                let photo = convertPhoto(from: photoResult)
+                completion(.success(photo))
+                DispatchQueue.main.async {
+                    self.photos.append(photo)
+                }
+                
+                NotificationCenter.default
+                    .post(
+                        name: ImagesListService.didChangeNotification,
+                        object: self
+                    )
+            case .failure(let error):
+                completion(.failure(error))
+            }
+            self.task = nil
+        }
+        self.task = task
+    }
+}
+
+private extension ImagesListService {
+    
+    func photoRequest() -> URLRequest? {
+        if var lastLoadedPage = lastLoadedPage {
+            lastLoadedPage += 1
+        } else {
+            lastLoadedPage = 1
+        }
+        
+        guard var urlComponents = URLComponents(string: Constants.defaultBaseURL) else {
+            assertionFailure("Failed to get URL from String")
+            return nil
+        }
+        urlComponents.path = "/photos"
+        urlComponents.queryItems = [URLQueryItem(name: "page", value: String(lastLoadedPage ?? 1))]
+        
+        guard let url = urlComponents.url else {
+            assertionFailure("Failed to create URL")
+            return nil
+        }
+        
+        var request = URLRequest(url: url)
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        return request
+    }
+    
+    func convertPhoto(from photoResult: PhotoResult) -> Photo {
+        Photo(
+            id: photoResult.id,
+            size: CGSize(width: photoResult.width, height: photoResult.height),
+            createdAt: ISO8601DateFormatter().date(from: photoResult.createdAt),
+            welcomeDescription: photoResult.description,
+            thumbImageURL: photoResult.urls.thumb,
+            largeImageURL: photoResult.urls.full,
+            isLiked: photoResult.likedByUser
+        )
+    }
+}
