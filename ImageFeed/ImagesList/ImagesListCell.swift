022d24555777f6b134cbdcd13fbbc8e7512677ec
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2023-05-20 23:55:00 +0300

    Implemented the showing of photos in ImagesListVC according to the contents of ImagesListService

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Kingfisher
 
 final class ImagesListCell: UITableViewCell {
     
@@ -17,10 +18,29 @@ extension ImagesListCell {
     
     // MARK: - Methods
     
-   func configure(image: UIImage?, date: String, isLiked: Bool) {
-        cellImage.image = image
-        cellImage.contentMode = .scaleAspectFill
-        dateLabel.text = date
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        cellImage.kf.cancelDownloadTask()
+    }
+    
+    func configure(from photos: [Photo], in tableView: UITableView, with indexPath: IndexPath) {
+        guard
+            let imagesListURL = photos[indexPath.row].thumbImageURL,
+            let url = URL(string: imagesListURL),
+            let date = photos[indexPath.row].createdAt,
+            let isLiked = photos[indexPath.row].isLiked
+        else { return }
+        
+        cellImage.kf.indicatorType = .activity
+        cellImage.kf.setImage(with: url, placeholder: UIImage(named: "CardStub")
+        ) { [weak self] _ in
+            guard let self else { return }
+            self.cellImage.kf.indicatorType = .none
+            self.cellImage.contentMode = .scaleAspectFill
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+
+        dateLabel.text = date.dateTimeString
         
         let likeImage = isLiked ? UIImage(named: "LikeButtonActive") : UIImage(named: "LikeButtonNoActive")
         likeButton.setImage(likeImage, for: .normal)
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -8,24 +8,56 @@ final class ImagesListViewController: UIViewController {
     
     // MARK: - Properties
     
-    private let photosName: [String] = Array(0...24).map{ "\($0)" }
     private let showSingleImageSegueIdentifier = "PresentSingleImage"
+    private let imagesListService = ImagesListService.shared
+    private var photos: [Photo] = []
+    private var imagesListServiceObserver: NSObjectProtocol?
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        
+        imagesListServiceObserver = NotificationCenter.default
+            .addObserver(
+                forName: ImagesListService.didChangeNotification,
+                object: nil,
+                queue: .main
+            ) { [weak self] _ in
+                guard let self else { return }
+                self.updateTableViewAnimated()
+            }
+        imagesListService.fetchPhotosNextPage { _ in }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == showSingleImageSegueIdentifier {
             guard let viewController = segue.destination as? SingleImageViewController,
                   let indexPath = sender as? IndexPath else { return }
-            let image = UIImage(named: photosName[indexPath.row])
+            let image = UIImage()
             viewController.image = image
         } else {
             super.prepare(for: segue, sender: sender)
+        }
+    }
+}
+
+private extension ImagesListViewController {
+    func updateTableViewAnimated() {
+        tableView.performBatchUpdates {
+            let oldCount = photos.count
+            let newCount = imagesListService.photos.count
+            photos = imagesListService.photos
+            if oldCount != newCount {
+                tableView.performBatchUpdates {
+                    var indexPaths: [IndexPath] = []
+                    (oldCount..<newCount).forEach { index in
+                        indexPaths.append(IndexPath(row: index, section: 0))
+                    }
+                    tableView.insertRows(at: indexPaths, with: .automatic)
+                } completion: { _ in }
+            }
         }
     }
 }
@@ -35,7 +67,7 @@ extension ImagesListViewController: UITableViewDataSource {
     // MARK: - TableViewDataSource methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        photosName.count
+        photos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -45,11 +77,7 @@ extension ImagesListViewController: UITableViewDataSource {
             return UITableViewCell()
         }
         
-        let image = UIImage(named: photosName[indexPath.row])
-        let date = Date().dateTimeString
-        let isLiked = indexPath.row % 2 == 0
-        imageListCell.configure(image: image, date: date, isLiked: isLiked)
-        
+        imageListCell.configure(from: photos, in: tableView, with: indexPath)
         return imageListCell
     }
 }
@@ -58,20 +86,22 @@ extension ImagesListViewController: UITableViewDelegate {
     
     // MARK: - TableViewDelegate methods
     
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == photos.count {
+            imagesListService.fetchPhotosNextPage { _ in }
+        }
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        guard let image = UIImage(named: photosName[indexPath.row]) else {
-            return 0
-        }
-        
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
-        let imageWidth = image.size.width
+        let imageWidth = photos[indexPath.row].size.width
         let scale = imageViewWidth / imageWidth
-        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        let cellHeight = photos[indexPath.row].size.height * scale + imageInsets.top + imageInsets.bottom
         return cellHeight
     }
 }
